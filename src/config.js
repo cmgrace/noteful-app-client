4a6b4d2e8588f1b763de5affd81bfc0d96e81d03
Ming Cheng <cmgrace@bu.edu>
2021-02-07 17:01:51 -0500

    deploy

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = "http://localhost:8000/api";
+//export const API_ENDPOINT = "http://localhost:8000/api";
+export const API_ENDPOINT = "https://guarded-cliffs-77483.herokuapp.com/api";
